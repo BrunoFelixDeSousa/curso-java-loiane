6ed94ad17094a0b43872fe843ab4e8b86ca64be9
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-04-05 00:52:43 -0300

    iniciando revisao de estudos com o curso de java da loiane no youtuber

---
+++ src/com/loiane/cursoJava/aula10/Variaveis.java
@@ -0,0 +1,33 @@
+package com.loiane.cursoJava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		// convenção Java
+		int idade = 20;
+		String nome = "Bruno";
+		String nomeDoMeuCachorro = "Apolo";
+		String ano2022 = "2022";
+
+		// aceito, mas não utilizado
+		int _idade;
+		int $idade;
+
+		// não é convenção Java
+		String nome_do_meu_cachorro;
+		String NomeDoMeuCachorro;
+		String nomeDoMeucachorro;
+
+		idade = 25;
+
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+
+		// má prática
+		int a = 10;
+		String b = "Bruno Félix";
+
+	}
+
+}
