6a879a92e2ce10bb3e782afbe8c7628a75407239
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-04-05 21:30:58 -0300

    add aula 31 - 35

---
+++ src/com/loiane/cursoJava/aula34/labs/Contador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loiane.cursoJava.aula34.labs;
+
+public class Contador {
+    
+    private static int cont;
+
+    public Contador() {
+        cont++;
+    }
+
+    public static void incrementar(){
+        cont++;
+    }
+    
+    public static void zerar(){
+        cont = 0;
+    }
+    
+    public static int obterValor(){
+        return cont;
+    }
+}
